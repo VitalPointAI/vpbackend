16ca4397b26dc77c01d81a3f3ef4aadb800bc590
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-04-04 13:00:57 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+
+// Azure authentication library to access Azure Key Vault
+const { DefaultAzureCredential } = require("@azure/identity")
+const { SecretClient } = require("@azure/keyvault-secrets")
+
+// Azure SDK clients accept the credential as a parameter
+const credential = new DefaultAzureCredential()
+let vaultUrl = 'https://vitalpointkeys.vault.azure.net/'
+
+// Create authenticated client
+const client = new SecretClient(vaultUrl, credential)
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/appseed', async (req, res) => {
+  let getResult
+  try{
+  getResult = await client.getSecret("APPSEED")
+  } catch (err) {
+    console.log('error retrieving secret', err)
+  }
+  res.send(getResult);
+ });
+
+ app.get('/didkey', async (req, res) => {
+  let getResult
+  try{
+  getResult = await client.getSecret("DIDCONTRACTPRIVKEY")
+  } catch (err) {
+    console.log('error retrieving secret', err)
+  }
+  res.send(getResult);
+ });
+
+app.listen(process.env.PORT || 8080);
