648dcbb1977d1c9b480423786b7459b8660b0f76
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-04-04 18:32:29 -0400

    remove cors

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const compression = require('compression')
 const path = require('path')
-const cors = require('cors')
 const app = express()
 const helmet = require('helmet');
 
@@ -19,20 +18,19 @@ const client = new SecretClient(vaultUrl, credential)
 
 //app.use(express.static(path.join(__dirname, 'dist')));
 
-const corsOptions = {
-  origin: 'https://nearpersonas.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 app.use(helmet());
 app.use(compression()); //Compress all routes
 
 app.use(function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "https://nearpersonas.com"); // update to match the domain you will make the request from
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  // next();
+  res.header("Access-Control-Allow-Origin", "https://nearpersonas.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
-app.get('/appseed', cors(corsOptions), async (req, res, next) => {
+app.get('/appseed', async (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://nearpersonas.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
   let getResult
   try{
   getResult = await client.getSecret("APPSEED")
@@ -42,7 +40,10 @@ app.get('/appseed', cors(corsOptions), async (req, res, next) => {
   res.send(getResult);
  });
 
- app.get('/didkey', cors(corsOptions), async (req, res, next) => {
+ app.get('/didkey', async (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://nearpersonas.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
   let getResult
   try{
   getResult = await client.getSecret("DIDCONTRACTPRIVKEY")
