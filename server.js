98f8b3518816f166c5b2f109ba502a0ffbc81e68
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-04-04 19:33:44 -0400

    wild

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(compression()); //Compress all routes
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://nearpersonas.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Methods', 'GET');
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next()
