3cc10ba12d9127ab20c6ad085d0dc6bb15369d9a
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-04-05 12:37:38 -0400

    fix next

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(compression()); //Compress all routes
 app.use(cors({
   origin: 'https://nearpersonas.com'
 }));
- app.use(function(req, res) {
+ app.use(function(req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "nearpersonas.com"); // update to match the domain you will make the request from
 //   res.header('Access-Control-Allow-Methods', 'GET');
 //   res.header("Access-Control-Allow-Headers", "Content-Type");
