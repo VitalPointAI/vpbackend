a3ea129610f5506ab9d016c0b4534a2a1ad83999
Aaron Luhning <a.luhning@vitalpoint.ai>
2021-08-05 07:56:24 -0600

    cors

--- server.js
+++ server.js
@@ -1,50 +1,16 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const path = require('path')
-const app = express()
-const axios = require('axios').default
 const cors = require('cors')
+const app = express()
 
-app.use(cors({
-  origin: 'https://catalystdao.com'
-}))
-
-//  app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://nearpersonas.com")
-//   res.header("Access-Control-Allow-Headers", "Origin, Content-Type,Content-Length, Authorization, Accept,X-Requested-With")
-//   res.header("Access-Control-Allow-Methods", "GET")
-//    next()
-//  });
+app.use(cors())
 
 app.get('/appseed', async (req, res) => {
-  let seed
-  try{
-    let code = process.env.CODE
-    seed = await axios.get('https://personas.azurewebsites.net/api/seed?code='+code)
-  } catch (err) {
-    console.log('error retrieving seed', err)
-    res.send('error')
-  }
- 
-  res.send(seed.data);
- });
-
- app.get('/didkey', async (req, res) => {
-  let seed
-  try{
-    let code = process.env.DID
-    seed = await axios.get('https://personas.azurewebsites.net/api/didkey?code='+code)
-  } catch (err) {
-    console.log('error retrieving seed', err)
-    res.send('error')
-  }
- 
-  res.send(seed.data);
+    let code = process.env.APP_SEED
+    res.send(code);
  });
 
  app.get('/airtable', async (req, res) => {
- // let airtable
- // try{
+
   const airtable = {
       airtableKey: process.env.AIRTABLE_KEY,
       contributorBase: process.env.CONTRIBUTOR_BASE,
@@ -52,11 +18,6 @@ app.get('/appseed', async (req, res) => {
       projectBase: process.env.PROJECT_BASE,
       projectTable: process.env.PROJECT_TABLE
   }
- //   seed = await axios.get('https://personas.azurewebsites.net/api/airtable?code='+airtableCode)
- // } catch (err) {
- //   console.log('error retrieving airtable data', err)
- //   res.send('error')
- // }
  
   res.send(airtable);
  });
